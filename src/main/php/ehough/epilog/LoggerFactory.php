a01481d62788aa86eee2ce8f3f000b02ea039428
Eric Hough <eric@ehough.com>
2013-03-21 01:28:43 -0400

    Updating copyright for LoggerFactory

--- src/main/php/ehough/epilog/LoggerFactory.php
+++ src/main/php/ehough/epilog/LoggerFactory.php
@@ -1,22 +1,11 @@
 <?php
-/**
- * Copyright 2012 Eric D. Hough (http://ehough.com)
+/*
+ * This file is part of the epilog package.
  *
- * This file is part of epilog (https://github.com/ehough/epilog)
+ * (c) Eric Hough <ehough.com>
  *
- * epilog is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * epilog is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with TubePress.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 /**
